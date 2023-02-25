ab2e9dfadf64d79249dd2dc5ffd91fda283a4de4
Andrew Grachev <dr.grachev@gmail.com>
2023-02-25 21:00:36 +0300

    Обновил ветку

---
+++ astra16/Keepalived-server/files/40/backup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+systemctl stop autofs
+systemctl stop nfs-server
+systemctl stop samba
+
+umount /mnt/shd/vol1
+umount /mnt/shd/vol2
+umount /mnt/shd/vol3
+umount /mnt/shd/vol4
+umount /mnt/shd/vol5
+umount /mnt/shd/vol6
+umount /mnt/shd/vol7
+umount /mnt/shd/vol8
+umount /mnt/shd/vol9
+umount /mnt/shd/vol10
+umount /mnt/shd/vol11
+umount /mnt/shd/vol12
+umount /mnt/shd/vol13
+umount /mnt/shd/vol14
+
+sleep 30
+
+systemctl rstart autofs
---
+++ astra16/SZI/files/security.sh
@@ -0,0 +1,10 @@
+astra-sumac-lock enable
+astra-ulimits-control enable
+#astra-nochmodx-lock enable
+astra-ptrace-lock enable
+astra-console-lock enable
+astra-sysrq-lock enable
+astra-commands-lock enable
+astra-sudo-control enable
+astra-swapwiper-control enable
+astra-secdel-control enable
